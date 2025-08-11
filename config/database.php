bfe73d63374e9151f8666d50eccd284d83fbf001
moh102moh <ahmad122sj@gmail.com>
2025-08-11 19:04:37 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -15,8 +15,9 @@ return [
     | is explicitly specified when you execute a query / statement.
     |
     */
+'default' => env('DB_CONNECTION', 'array'),
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+
 
     /*
     |--------------------------------------------------------------------------
